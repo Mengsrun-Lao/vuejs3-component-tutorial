1d0f02e5d036614282c83982044f3cfb9dd0518d
Mengsrun-Lao <lao.mengsrun8@gmail.com>
2024-10-26 09:11:46 +0700

    T39 - Slots props

---
+++ src/components/NameList.vue
@@ -0,0 +1,34 @@
+<template>
+    <h3 v-for="name in names" :key="name.firstName">
+        <slot :firstName="name.firstName" :lastName="name.lastName"></slot>
+    </h3>
+</template>
+
+<script>
+    export default {
+        name:'nameList-component',
+        data(){
+            return {
+                names:[
+                    {
+                        firstName: 'firstName-1111',
+                        lastName : 'lastName-1111'
+                    },
+                    {
+                        firstName: 'firstName-2222',
+                        lastName : 'lastName-2222'
+                    },
+                    {
+                        firstName: 'firstName-3333',
+                        lastName : 'lastName-3333'
+                    },
+ 
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
